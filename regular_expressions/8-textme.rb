6ffc7686e517ed632959bd3a04ce23a2cc9112d6
Becca4375 <r.adhiamboo@alustudent.com>
2026-07-05 23:07:28 -0700

    8-textme.rb

--- regular_expressions/8-textme.rb
+++ regular_expressions/8-textme.rb
@@ -1,2 +1,4 @@
 #!/usr/bin/env ruby
-puts ARGV[0].scan(/from:(.+)\]\s\[to:(.+)\]\s\[flags:(.+)\]\s\[ms/).join
+input = ARGV[0]
+m = input.match(/from:([^]]+).*to:([^]]+).*flags:([^]]+)/)
+puts "#{m[1]},#{m[2]},#{m[3]}" if m
